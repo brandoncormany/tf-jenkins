58868003067a557ab9d6707f7b099574b72633ba
Brandon Cormany <brandoncormany@gmail.com>
2018-03-30 15:52:07 -0700

    Fixing Name of EFS and adding EFS mount points per subnet.

--- efs.tf
+++ efs.tf
@@ -1,7 +1,22 @@
-resource "aws_efs_file_system" "jenkinsefs" {
-  creation_token = "jenkinsEFS"
+resource "aws_efs_file_system" "jenkins_efs" {
+  creation_token = "jenkins_efs"
 
   tags {
-    Name = "jenkinsEFS"
+    Name = "jenkins_efs"
   }
+}
+
+resource "aws_efs_mount_target" "publicA" {
+  file_system_id = "${aws_efs_file_system.jenkins_efs.id}"
+  subnet_id      = "${aws_subnet.publicA.id}"
+}
+
+resource "aws_efs_mount_target" "publicB" {
+  file_system_id = "${aws_efs_file_system.jenkins_efs.id}"
+  subnet_id      = "${aws_subnet.publicB.id}"
+}
+
+resource "aws_efs_mount_target" "publicC" {
+  file_system_id = "${aws_efs_file_system.jenkins_efs.id}"
+  subnet_id      = "${aws_subnet.publicC.id}"
 }
