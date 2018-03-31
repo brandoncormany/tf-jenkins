102be573dbec6e2df193c7a348c39f65100a6206
Brandon Cormany <brandoncormany@gmail.com>
2018-03-30 23:01:59 -0700

    fixing CIDR block for gateway

--- public_route_table.tf
+++ public_route_table.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "public_route" {
   vpc_id = "${aws_vpc.jenkins_vpc.id}"
 
 route {
-  cidr_block = "0.0.0.0"
+  cidr_block = "0.0.0.0/0"
   gateway_id = "${aws_internet_gateway.gw.id}"
 }
 
