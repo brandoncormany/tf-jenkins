0df742c07330352ab58ab8b674d3d47ec2918445
Brandon Cormany <brandoncormany@gmail.com>
2018-04-03 19:32:26 -0700

    fixing EFS mount points to use default SG

--- efs.tf
+++ efs.tf
@@ -5,16 +5,19 @@ resource "aws_efs_file_system" "jenkins_efs" {
 }
 
 resource "aws_efs_mount_target" "publicA" {
-  file_system_id = "${aws_efs_file_system.jenkins_efs.id}"
-  subnet_id      = "${aws_subnet.publicA.id}"
+  file_system_id  = "${aws_efs_file_system.jenkins_efs.id}"
+  subnet_id       = "${aws_subnet.publicA.id}"
+  security_groups = ["${aws_default_security_group.default.id}"]
 }
 
 resource "aws_efs_mount_target" "publicB" {
-  file_system_id = "${aws_efs_file_system.jenkins_efs.id}"
-  subnet_id      = "${aws_subnet.publicB.id}"
+  file_system_id  = "${aws_efs_file_system.jenkins_efs.id}"
+  subnet_id       = "${aws_subnet.publicB.id}"
+  security_groups = ["${aws_default_security_group.default.id}"]
 }
 
 resource "aws_efs_mount_target" "publicC" {
-  file_system_id = "${aws_efs_file_system.jenkins_efs.id}"
-  subnet_id      = "${aws_subnet.publicC.id}"
+  file_system_id  = "${aws_efs_file_system.jenkins_efs.id}"
+  subnet_id       = "${aws_subnet.publicC.id}"
+  security_groups = ["${aws_default_security_group.default.id}"]
 }
