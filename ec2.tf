2a9999addf258d9c0b86806d88eea61fb36c0f20
Brandon Cormany <brandoncormany@gmail.com>
2018-04-04 08:50:59 -0700

    adding ELB to both config and ASG and some fmt clean up

--- ec2.tf
+++ ec2.tf
@@ -8,13 +8,13 @@ data "template_file" "userdata" {
 }
 
 resource "aws_launch_configuration" "jenkinsLC" {
-  name_prefix     = "tf-jenkins-lc"
-  image_id        = "${lookup(var.ecs_ami, var.region)}"
-  instance_type   = "t2.large"
-  iam_instance_profile = "Ec2Role" //Needs to be preconfigured based on role created in IAM
-  security_groups = ["${aws_default_security_group.default.id}"]
-  user_data       = "${data.template_file.userdata.rendered}"
-  key_name        = "aws"
+  name_prefix          = "tf-jenkins-lc"
+  image_id             = "${lookup(var.ecs_ami, var.region)}"
+  instance_type        = "t2.large"
+  iam_instance_profile = "Ec2Role"                                    //Needs to be preconfigured based on role created in IAM
+  security_groups      = ["${aws_default_security_group.default.id}"]
+  user_data            = "${data.template_file.userdata.rendered}"
+  key_name             = "aws"
 
   root_block_device {
     volume_size = 8
@@ -26,6 +26,31 @@ resource "aws_launch_configuration" "jenkinsLC" {
   }
 }
 
+resource "aws_elb" "jenkinsELB" {
+  name            = "jenkinsELB"
+  subnets         = ["${aws_subnet.publicA.id}", "${aws_subnet.publicB.id}", "${aws_subnet.publicC.id}"]
+  security_groups = ["${aws_default_security_group.default.id}"]
+
+  listener {
+    instance_port     = 8080
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:8080"
+    interval            = 5
+  }
+
+  tags {
+    Name = "jenkins-elb"
+  }
+}
+
 resource "aws_autoscaling_group" "jenkinsASG" {
   name                 = "jenkinsASG"
   max_size             = 1
@@ -34,6 +59,7 @@ resource "aws_autoscaling_group" "jenkinsASG" {
   health_check_type    = "EC2"
   launch_configuration = "${aws_launch_configuration.jenkinsLC.name}"
   vpc_zone_identifier  = ["${aws_subnet.publicA.id}", "${aws_subnet.publicB.id}", "${aws_subnet.publicC.id}"]
+  load_balancers       = ["${aws_elb.jenkinsELB.name}"]
 
   tags = [
     {
