1fc3f2f63f542264f2ee10b7ad0b4365072e2897
Brandon Cormany <brandoncormany@gmail.com>
2018-04-04 08:50:40 -0700

    allowing TCP port 80 from eveywhere

--- vpc.tf
+++ vpc.tf
@@ -33,6 +33,13 @@ resource "aws_default_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
